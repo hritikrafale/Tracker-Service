deeb9df026fcc9eb1e73fb943f9b65807d9d5ecb
hritikrafale <hritikrafale@gmail.com>
2023-10-25 11:29:09 +0530

    Added slf4j logging

--- Tracker-Backend/expense/src/main/java/com/tracker/expense/controller/ExpenseController.java
+++ Tracker-Backend/expense/src/main/java/com/tracker/expense/controller/ExpenseController.java
@@ -2,6 +2,8 @@ package com.tracker.expense.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,18 +15,21 @@ import com.tracker.expense.service.ExpenseService;
 
 @RestController
 public class ExpenseController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(ExpenseController.class);
 
 	@Autowired
 	ExpenseService expenseService;
 	
 	@GetMapping("/all_expenses")
 	public List<Expense> getAllExpenses(){
+		logger.info("inside getAllExpenses() method of expense controller");
 		return expenseService.getAllExpenses();
 	}
 	
 	@PostMapping("/new_expense")
 	public Expense saveExpense(@RequestBody Expense expense) {
-		System.out.println("Expense : ");
+		logger.info("inside saveExpense() method of expense controller, expense is {}",expense);
 		return expenseService.saveExpense(expense);
 	}
 	
--- Tracker-Backend/expense/src/main/java/com/tracker/expense/service/impl/ExpenseServiceImpl.java
+++ Tracker-Backend/expense/src/main/java/com/tracker/expense/service/impl/ExpenseServiceImpl.java
@@ -2,6 +2,8 @@ package com.tracker.expense.service.impl;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,17 +13,21 @@ import com.tracker.expense.service.ExpenseService;
 
 @Service
 public class ExpenseServiceImpl implements ExpenseService {
+	
+	private static final Logger logger = LoggerFactory.getLogger(ExpenseServiceImpl.class);
 
 	@Autowired
 	ExpenseRepository expenseRepo;
 	
 	@Override
 	public List<Expense> getAllExpenses() {
+		logger.info("inside getAllExpenses() method of expense service class");
 		return expenseRepo.findAll();
 	}
 
 	@Override
 	public Expense saveExpense(Expense expense) {
+		logger.info("inside saveExpense() method of expense service");
 		return expenseRepo.save(expense);
 	}
 
